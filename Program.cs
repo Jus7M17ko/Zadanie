7de583d8a62fcaf15c00f515e76c45e579242ca7
Jus7M17ko <markovdimitr98@gmail.com>
2024-03-13 17:56:41 +0200

    Upr1

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("gender");
+var gender = Console.ReadLine();
+Console.WriteLine("age");
+var age = int.Parse(Console.ReadLine());
+
+if (gender == "m" && age <= 18)
+{
+    Console.WriteLine("Boy");
+}
+else if (gender == "m" && age > 18)
+{
+    Console.WriteLine("Mr");
+}
+if (gender == "f" && age <= 18);
+{
+    Console.WriteLine("Girl");
+}
+if (gender == "f" && age > 18)
+{
+    Console.WriteLine("Ms");
+}
